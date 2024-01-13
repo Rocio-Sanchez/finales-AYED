6b71ef8766efde1ebf2a5c2b62ef115f6eb2c499
Rocio-Sanchez <rocsanchez@frba.utn.edu.ar>
2024-01-13 17:33:09 -0300

    Preparando primer final

---
+++ bodega.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct PedidosDelMes
+{
+    int localidad = 0;
+    int caja[9];
+    int idChofer = 0;
+};
+
+int info_PedidoxDestxTipoVino( PedidosDelMes vectorDePedidos[] );
+
+main(){
+
+    int precios[9];
+    int distancia[1000];
+    int n;
+    PedidosDelMes pedidosDelMes[n];
+}
+
+
+// Asumiendo que las locaciones no se repiten.
+int info_PedidoxDestxTipoVino( PedidosDelMes vectorDePedidos[] ) {
+
+
+};
